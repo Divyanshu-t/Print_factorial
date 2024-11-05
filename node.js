59c960aca7cc5bb77c2e64b678782724367cfd50
Smily-zack <155127760+Smily-zack@users.noreply.github.com>
2024-11-05 12:14:52 -0500

    first comment

---
+++ node.js
@@ -0,0 +1,12 @@
+
+// Print Factorial
+
+let user_input = parseInt(prompt("Enter Any Number")); 
+let f = 1; 
+
+for (let i = user_input; i >= 1; i--) { 
+}
+
+document.write(`The Factorial of ${user_input} is ${f}`); 
+
+
